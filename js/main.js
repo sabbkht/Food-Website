63f7cde51ecddea4e391f3510716e5f74e9701d0
sabbkht <sbakhtiari086@gmail.com>
2026-07-20 12:08:08 +0330

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("fa-bars");
+    menu.classList.toggle("fa-xmark");
+    navbar.classList.toggle("active");
+};
+
+let section = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header .navbar a');
+
+
+window.onscroll = () => {
+    menu.classList.remove("fa-bars");
+    menu.classList.remove("fa-xmark");
+    navbar.classList.remove("active");
+
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLink.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+        };
+    })
+};
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+
+
+ var swiper = new Swiper(".home-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop:true,
+    });
+
+
+     var swiper = new Swiper(".review-slider", {
+      spaceBetween: 20,
+      centeredSlides: true,
+      autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+      },
+      loop:true,
+      breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        640:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+      },
+    });
+
+    function loader(){
+        document.querySelector('.loader-container').classList.add('fade-out')
+    }
+
+    function fadeOut(){
+        setTimeout(loader, 3000);
+    }
+
+    window.onload = fadeOut;
